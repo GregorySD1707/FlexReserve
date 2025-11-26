0ef409e7bf0099939b46210212757426258129e2
sebas <sebastian11leon@gmail.com>
2025-11-26 10:25:07 -0500

    Add app.css stylesheet to multiple views for consistent styling

--- resources/views/VistaInicioSesion.blade.php
+++ resources/views/VistaInicioSesion.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesión - FlexReserve</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <h1>FlexReserve</h1>
